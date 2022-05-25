d190a13749231e12185f3f2f353a52c3dd976ba9
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2022-05-25 12:50:36 +0200

    adds transfer function generation script

---
+++ generate_TF.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+ANALOG_ATTN = [4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9]   # in dB
+DIGITAL_ATTN = [4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9]  # in dB
+WRF = 2518300671.117578       #: RF frequency (angular)
+
+
+class Generate_TF(object):
+
+    def __init__(self):
+
+        self.frequency = np.linspace(-1.5e6, 1.5e6, 5000)
+        self.closed_loop_response(self.frequency)
+        self.bode_plot()
+
+    def closed_loop_response(self, frequency):
+
+        # Use measured value
+        self.fb_attn_index = 3 # depends on cavity
+        self.analog_attn = ANALOG_ATTN[self.fb_attn_index]
+        self.digital_attn = DIGITAL_ATTN[self.fb_attn_index]
+
+#        g_a = prm.analog.gain*analog_attn
+#        g_d = (prm.digital.gain/g_a)*digital_gain*digital_attn
+
+        feedback_model = self.feedback_TF(frequency,
+            tau_a=170e-6,  #prm.analog.tau,
+            tau_d=400e-6, #prm.digital.tau,
+            g_a=6.8e-6, #g_a,
+            g_d=10, #g_d
+#            delta_phi=prm.digital.phase,
+        )
+
+        cavity_model = self.cavity_TF(frequency,
+            delta_w=6e3, #prm.cavity.wr - self.WRF,
+            g_oo=2e-3,  # init guess, was 1e-2
+            q=60000, #q_cl,
+            w_rf=WRF
+        )
+
+        phase_model = self.phase_TF(frequency,
+            phi=-10,  # init guess, was 50
+            tau=650e-9 #prm.delay.loop,
+        )
+
+        tf_model = feedback_model*cavity_model*phase_model
+        self.cl_response = tf_model/(1 - tf_model)
+
+
+    def feedback_TF(self, frequency, tau_a, tau_d, g_a, g_d, delta_phi=0):
+        w = 2*np.pi*frequency  # omega
+
+        # fraction 1: \\frac{G_{D} e^{\\Delta\\varphi \\frac{\\pi}{180} i}}{
+        # 1+\\omega \\tau_{D} i}
+        # Digital FB
+        num1 = g_d*np.exp(np.radians(delta_phi)*1j)
+        den1 = 1 + w*tau_d*1j
+
+        # fraction 2: \\frac{\\tau_{A} \\omega}{\\tau_{A} \\omega-i}
+        # Analog FB
+        num2 = tau_a*w
+        den2 = tau_a*w - 1j
+
+        resp = -g_a*(num1/den1 + num2/den2)
+
+        return resp
+
+
+    def cavity_TF(self, frequency, delta_w, g_oo, q, w_rf, r_over_q=45):
+        w = 2*np.pi*frequency  # omega
+        s = 1j*(w + w_rf)
+
+        w_r = w_rf + delta_w  # resonant frequency
+
+        resp = g_oo*r_over_q*w_r*s/np.sqrt(q)/(
+                    s**2 + (w_r/q)*s + w_r**2)
+
+        return resp
+
+    def phase_TF(self, frequency, phi, tau):
+
+        w = 2*np.pi*frequency  # omega
+
+        resp = np.exp(phi*np.pi/180*1j)*np.exp((-1)*w*tau*1j)
+
+        return resp
+
+    def bode_plot(self):
+
+        amplitude_linear = np.absolute(self.cl_response)
+        phase = np.angle(self.cl_response)
+
+        amplitude_dB = 20*np.log10(amplitude_linear)
+
+
+        plt.figure("Amplitude")
+        plt.plot(self.frequency, amplitude_dB)
+
+        plt.figure("Phase")
+        plt.plot(self.frequency, phase)
+        plt.show()
+
+
+#def main():
+
+
+#if __name__ == "__main__":
+
+Generate_TF()
